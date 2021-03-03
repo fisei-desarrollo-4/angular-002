a8991ec61a1d741b30b4daae4cec28fa3bcb0b81
Clay Aldas <clayaldas@gmail.com>
2021-03-03 17:01:02 -0500

    Directivas: *ngFor, *ngIf.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular002';
+
+  listaEstudiante: any[] = [
+    {
+      nombre: 'John Doe',
+      estado: 'Aprobado',
+    },
+    {
+      nombre: 'Mary Jhonson',
+      estado: 'Aprobado',
+    },
+    {
+      nombre: 'Luis Torrega',
+      estado: 'Suspenso',
+    },
+  ];
+
+  mostrar: boolean = true;
+
+  ejecutar(): void {
+    this.mostrar = !this.mostrar;
+  }
 }
